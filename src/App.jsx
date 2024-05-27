b6916c107388615e5e00aefaa75babc85bd19a74
Ardi Saputra <ardi210303@gmail.com>
2024-05-27 11:44:51 +0700

    change folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardPage from "@/pages/Dashboard";
+import DashboardPage from "@/pages/dashboard";
 
 function App() {
   return (
