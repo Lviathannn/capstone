26042b3f563486f10dd7e123a565693fcbafb5d9
Muhammad Asrul Rifa Anwar <muhammad.asrul.rifa@gmail.com>
2024-05-27 11:11:00 +0700

    feat : init shadcn ui and create simple login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "@/pages/landing";
+import { LoginPage } from "@/pages/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/landing/LandingTentangkami.jsx
+++ src/pages/landing/LandingTentangkami.jsx
@@ -1,23 +1,37 @@
-import About from '../../assets/img/about.png';
+import About from "../../assets/img/about.png";
 
 export default function LandingTentangkami() {
-    return (
-        <div className='container mx-auto py-[30px]'>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 py-[105px]">
-                <div className="flex justify-center">
-                    <img src={About} alt="About Us" className="w-full h-auto max-w-md md:max-w-full" />
-                </div>
-                <div className="flex flex-col justify-center px-5 py-20">
-                    <h2 className="text-4xl font-bold text-primary-500 mb-[30px] font-sans">Tentang Kami</h2>
-                    <p className="text-base font-medium text-neutral-900 font-sans">
-                        TourEase adalah aplikasi mobile inovatif yang menyediakan informasi lengkap tentang berbagai destinasi wisata, akses rute perjalanan terbaik, dan asisten AI cerdas untuk membantu merencanakan perjalanan Anda.
-                        <br /><br />
-                        Dengan TourEase, Anda bisa menemukan tempat-tempat menarik, mendapatkan petunjuk arah yang akurat, dan menerima rekomendasi yang dipersonalisasi untuk pengalaman wisata yang lebih menyenangkan dan efisien.
-                        <br /><br />
-                        Jelajahi dunia dengan mudah dan menyenangkan bersama TourEase!
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="container mx-auto py-[30px]">
+      <div className="grid grid-cols-1 gap-6 py-[105px] md:grid-cols-2">
+        <div className="flex justify-center">
+          <img
+            src={About}
+            alt="About Us"
+            className="h-auto w-full max-w-md md:max-w-full"
+          />
         </div>
-    );
+        <div className="flex flex-col justify-center px-5 py-20">
+          <h2 className="text-primary-500 mb-[30px] text-4xl font-bold ">
+            Tentang Kami
+          </h2>
+          <p className="text-base font-medium text-neutral-900 ">
+            TourEase adalah aplikasi mobile inovatif yang menyediakan informasi
+            lengkap tentang berbagai destinasi wisata, akses rute perjalanan
+            terbaik, dan asisten AI cerdas untuk membantu merencanakan
+            perjalanan Anda.
+            <br />
+            <br />
+            Dengan TourEase, Anda bisa menemukan tempat-tempat menarik,
+            mendapatkan petunjuk arah yang akurat, dan menerima rekomendasi yang
+            dipersonalisasi untuk pengalaman wisata yang lebih menyenangkan dan
+            efisien.
+            <br />
+            <br />
+            Jelajahi dunia dengan mudah dan menyenangkan bersama TourEase!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
