afc504439b263825214a7614617ec8e5dfa2f99e
Ardi Saputra <ardi210303@gmail.com>
2024-06-10 21:17:31 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,13 @@ import UserEdit from "@/pages/ManageUser/UserEdit";
 import { Toaster } from "@/components/ui/sonner";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import ManageRoute from "@/pages/ManageRoute";
 
 function App() {
   const currentUser = useSelector((state) => state.auth.user);
 
   return (
+<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path="/edit" element={<EditAdmin />} />
@@ -53,6 +55,38 @@ function App() {
         <Toaster />
       </BrowserRouter>
 
+=======
+    <BrowserRouter>
+      <Routes>
+        {/* Protected Routes */}
+        <Route
+          path="/detail"
+          element={currentUser ? <DetailAdmin /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/add"
+          element={currentUser ? <AddAdmin /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/manage-admin"
+          element={currentUser ? <DisplayAdmin /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/dashboard"
+          element={currentUser ? <DashboardPage /> : <Navigate to="/login" />}
+        />
+        {/* Public Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/manage-user" element={<ManageUser />} />
+        <Route path="/manage-user/detail" element={<UserDetail />} />
+        <Route path="/manage-user/create" element={<UserCreate />} />
+        <Route path="/manage-route" element={<ManageRoute />} />
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
+>>>>>>> f234909 (feat: manage route page (get route data))
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "@/lib/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+<<<<<<< HEAD
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from "react-query";
+=======
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+>>>>>>> f234909 (feat: manage route page (get route data))
 
 const queryClient = new QueryClient();
 
@@ -16,9 +22,14 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+<<<<<<< HEAD
       <QueryClientProvider client={queryClient}>
           <App />
           <ReactQueryDevtools initialIsOpen={false} client={queryClient}/>
+=======
+        <QueryClientProvider client={queryClient}>
+          <App />
+>>>>>>> f234909 (feat: manage route page (get route data))
         </QueryClientProvider>
       </PersistGate>
     </Provider>
