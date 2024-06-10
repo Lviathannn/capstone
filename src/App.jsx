b96fa443fcea364d73863dcc5b6f129257d28e03
Ardi Saputra <ardi210303@gmail.com>
2024-06-10 21:35:36 +0700

    fix:route

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { LoginPage } from "@/pages/login";
 import { DisplayAdmin } from "@/pages/ManageAdmin/DisplayAdmin/index";
 import { EditAdmin } from "@/pages/ManageAdmin/EditAdmin/index";
 
-import DashboardPage from "@/pages/Dashboard";
+import DashboardPage from "@/pages/dashboard";
 import ManageUser from "@/pages/ManageUser";
 import UserDetail from "@/pages/ManageUser/UserDetail";
 import UserCreate from "@/pages/ManageUser/UserCreate";
@@ -23,40 +23,6 @@ function App() {
   const currentUser = useSelector((state) => state.auth.user);
 
   return (
-<<<<<<< HEAD
-      <BrowserRouter>
-        <Routes>
-          <Route path="/edit" element={<EditAdmin />} />
-          {/* Protected Routes */}
-          <Route
-            path="/detail/:id"
-            element={currentUser ? <DetailAdmin /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/add"
-            element={currentUser ? <AddAdmin /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/manage-admin"
-            element={currentUser ? <DisplayAdmin /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/dashboard"
-            element={currentUser ? <DashboardPage /> : <Navigate to="/login" />}
-          />
-          {/* Public Routes */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/manage-user" element={<ManageUser />} />
-          <Route path="/manage-user/detail" element={<UserDetail />} />
-          <Route path="/manage-user/create" element={<UserCreate />} />
-          <Route path="/manage-user/edit" element={<UserEdit />} />
-        </Routes>
-        <Toaster />
-      </BrowserRouter>
-
-=======
     <BrowserRouter>
       <Routes>
         {/* Protected Routes */}
@@ -88,7 +54,6 @@ function App() {
       </Routes>
       <Toaster />
     </BrowserRouter>
->>>>>>> f234909 (feat: manage route page (get route data))
   );
 }
 
--- src/pages/ManageRoute/TotalRoute.jsx
+++ src/pages/ManageRoute/TotalRoute.jsx
@@ -1,9 +1,9 @@
-import person from "@/assets/icons/person.png";
+import { PersonIcon } from "@/assets/icons";
 
 export const TotalRoute = ({ filteredData }) => {
   return (
     <div className="items-left col-span-4 flex flex-col justify-center rounded-lg bg-neutral-50 p-4 lg:col-span-2">
-      <img src={person} alt="Person Icon" className="mb-4 h-6 w-6" />
+      <PersonIcon className="h-6 w-6 mb-4" />
       <p className="font-jakarta-sans text-[26px] font-[700] text-neutral-800">
         {filteredData?.length}
       </p>
