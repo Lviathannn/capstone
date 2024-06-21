5ffed6c45b30598d6def00a799ef1c0d2844c638
Ardi Saputra <ardi210303@gmail.com>
2024-06-21 20:15:46 +0700

    feat: search with useDebounce & skeleton

--- src/components/features/skeleton/TableSkeleton.jsx
+++ src/components/features/skeleton/TableSkeleton.jsx
@@ -1,36 +1,14 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { TableCell, TableRow } from "@/components/ui/table";
 
-export default function TableSkeleton() {
+export default function TableSkeleton({ tableCell }) {
   return (
     <TableRow>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
-      <TableCell className="text-nowrap">
-        <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
-      </TableCell>
+      {Array.from({ length: tableCell }).map((_, index) => (
+        <TableCell key={index} className="text-nowrap">
+          <Skeleton className="h-5 w-full rounded-lg bg-neutral-200" />
+        </TableCell>
+      ))}
     </TableRow>
   );
 }
